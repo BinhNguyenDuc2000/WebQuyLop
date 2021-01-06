95f525d6ee0d2c15ffefe57b1588afe376d16959
bcoldx <76272856+bcoldx@users.noreply.github.com>
2021-01-06 16:22:04 +0700

    file sql Csdl

---
+++ Database.sql
@@ -0,0 +1,10 @@
+﻿create table SinhVien(
+ MSSV int not null primary key,
+ HoTen nvarchar(30) not null,
+ TenLop nvarchar(30) not null default N'Việt Nhật 4',
+ Khoa int not null default 63,
+ SDT varchar(15),
+ Email varchar(30),
+ QuyenAdmin int not null check(QuyenAdmin>=0 and QuyenAdmin<=2) default 1,
+ Pass varchar(30) not null default '123456'
+)
