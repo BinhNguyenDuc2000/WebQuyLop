f87bdfd5bfdabc5a2b5a9e21ea88c4c8dadaabab
tranhoanglonghai2k <76715766+tranhoanglonghai2k@users.noreply.github.com>
2021-01-14 16:08:42 +0700

    sửa

--- Database.sql
+++ Database.sql
@@ -1,14 +1,45 @@
-﻿create table SinhVien(
- MSSV int not null primary key,
- HoTen nvarchar(30) not null,
- TenLop nvarchar(30) not null default N'Việt Nhật 4',
- Khoa int not null default 63,
- SDT varchar(15),
- Email varchar(30) Unique,
- QuyenAdmin int not null check(QuyenAdmin>=0 and QuyenAdmin<=2) default 1,
- Pass varchar(100) not null default '123456'
-)
- drop table SinhVien
+﻿create table [SinhVien] (
+	[MSSV] int not null,
+	[HoTen] nvarchar(50) not null,
+	[Lop] nvarchar(20) not null default N'Việt Nhật 4',
+	[Khoa] int not null default 63,
+	[NgaySinh] date not null,
+	[SoDienThoai] int not null,
+	[DiaChi] varchar(40) not null,
+	[Email] varchar(100) Unique,
+	[QuyenAdmin] int not null check(QuyenAdmin>=0 and QuyenAdmin<=2) default 1,
+	[Pass] varchar(30) not null default '123456',
+	primary key ([MSSV]));
+
+create table [QuyLop] (
+	[MaLop] int not null,
+	[TenLop] varchar(20) not null,
+	[TienQuy] int not null,
+	primary key ([MaLop]));
+
+create table [SuKien] (
+	[MaSK] int not null,
+	[TenSK] varchar(100) not null,
+	[Ngay] date not null,
+	[GhiChu] varchar(1000) not null,
+	primary key ([MaSK]));
+
+create table [Dong] (
+	[MSSV] int not null,
+	[MaLop] int not null,
+	[SoTien] int not null,
+	primary key ([MSSV], [MaLop]),
+	foreign key ([MSSV]) references [SinhVien]([MSSV]),
+	foreign key ([MaLop]) references [QuyLop]([MaLop]));
+
+create table [Chi] (
+	[MaLop] int not null,
+	[MaSK] int not null,
+	[SoTienChi] int not null,
+	primary key ([MaLop], [MaSK]),
+	foreign key ([MaLop]) references [QuyLop]([MaLop]),
+	foreign key ([MaSK]) references [SuKien]([MaSK]));
+drop table SinhVien
  insert into SinhVien
  values ('1',N'1',N'1','1','1','1@1','1','1')
   
